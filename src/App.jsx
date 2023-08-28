ea8492a424dc4aa7a1b8e3a85e4bfcd0b209d940
antoluduenabereziuk21 <antolube20@gmail.com>
2023-08-28 15:01:36 -0300

    refactor app.js & create component Movies

--- src/App.jsx
+++ src/App.jsx
@@ -2,42 +2,13 @@ import "./App.css";
 
 import responseMovies from "./mocks/with-results.json";
 import withoutResults from "./mocks/no-results.json";
+import { Movies } from "./components/Movies";
 
 function App() {
-  //consultamos si tenemos resultados con search
-  const movies = responseMovies.Search;
-  //sabiendo que nos devuelve un array
-  //preguntamos si ese array es mayor a 0
-  const hasMovies = movies?.length > 0;
   
-  //para luego en el main renderizar mediante
-  //renderizado condicional, para luego llevarlo a un componente
-/*
+  const movies = responseMovies.Search;
+ 
 
-Esto resulta una mala practica, porque deberia ser un componente
-Cualquier funcion que renderize algo debe ser un Componente
-
-  const renderMovies= ()=>{
-      return(
-        <ul>
-            {movies.map((movie) => (
-              <li key={movie.imdbID}>
-                <h3>{movie.Title}</h3>
-                <p>{movie.Year}</p>
-                <img src={movie.Poster} alt={movie.Title} />
-              </li>
-            ))}
-          </ul>
-      )
-
-  }
-
-  const renderNoResults = ()=>{
-    return(
-      <p>No se encontro resultados en la busqueda</p>
-    )
-  }
-*/
   return (
     <div className="page">
       <header>
@@ -48,9 +19,7 @@ Cualquier funcion que renderize algo debe ser un Componente
         </form>
       </header>
       <main>
-        {hasMovies 
-        ? renderMovies() 
-        : renderNoResults()}
+        <Movies movies={movies}/>
       </main>
     </div>
   );
