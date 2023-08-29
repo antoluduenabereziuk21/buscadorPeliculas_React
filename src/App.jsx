28bbc0553a4dd06341b54d2c5822a18f62b7ec59
antoluduenabereziuk21 <antolube20@gmail.com>
2023-08-28 21:41:19 -0300

    uncontrolled form data retrieval

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,39 @@
 import "./App.css";
 import { useMovies } from "./Hooks/useMovies";
 import { Movies } from "./components/Movies";
-import { useRef } from "react";
-/**
- * use ref te permite guardar una referencia mutable
- * que perciste durante todo el ciclo de vida del 
- * componente.Es muy util para guardar cualkeir valor que 
- * puedas guardar , y cada vez que cambia no renderiza
- * nuevamente el componente
- */
 
 function App() {
   const { movies: mappedMovies } = useMovies();
-  //lo utilizaremos para guardar una referencia del DOM
-  //en este caso lo usaremos en el input del form
-  const inputRef = useRef();
-  //cuando se haga click se llamara al handle 
-  //y sacaremos el valor de la referencia
+/**
+ * 
+ * Este tipo de recuperacionde informacion de los inputs
+ *  se conoce como no controlada , es la forma mas sencilla
+ * y mas optima
+ */
+  const handleSubmit = (e) => {
+
+    e.preventDefault();
+    const fields = new window.FormData(e.target);
+    const query = fields.get("query");
+    console.log(query);
+    
+  };
+
+  /*
+  otra alternativa es aglomerar los fields en un objeto
   const handleSubmit = (e)=>{
     e.preventDefault();
-    const value = inputRef.current.value;
-    console.log(value);
+    const fields = Object.fromEntries(new window.FormData(e.target));
+    console.log(fields);
   };
-  //pero utilizaremos el onSubmit en el form que tiene
-  //mayores ventajas
-
-
+*/
   return (
     <div className="page">
       <header>
         <h1>Buscador de péliculas</h1>
         <form className="form" onSubmit={handleSubmit}>
-          <input ref={inputRef} placeholder="Avengers, Star Wars, The Matrix" />
-          <button  type="submit">Buscar</button>
+          <input name="query" placeholder="Avengers, Star Wars, The Matrix" />
+          <button type="submit">Buscar</button>
         </form>
       </header>
       <main>
