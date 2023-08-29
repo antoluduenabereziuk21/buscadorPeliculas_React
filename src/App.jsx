9e72f69a15d119419ee52eee177a6258f3335860
antoluduenabereziuk21 <antolube20@gmail.com>
2023-08-29 10:14:29 -0300

    implements handle control form

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,38 @@
+import { useState } from "react";
 import "./App.css";
 import { useMovies } from "./Hooks/useMovies";
 import { Movies } from "./components/Movies";
 
 function App() {
   const { movies: mappedMovies } = useMovies();
-/**
- * 
- * Este tipo de recuperacionde informacion de los inputs
- *  se conoce como no controlada , es la forma mas sencilla
- * y mas optima
- * 
- * //sot para manejar validacionde formularios
- */
-  const handleSubmit = (e) => {
+  
+  const [query, setQuery] = useState('');
 
-    e.preventDefault();
-    const fields = new window.FormData(e.target);
-    const query = fields.get("query");
-    console.log(query);
-    
-  };
+  console.log('render');
+  /**
+   * en la state almacenaremos la query que se valla cargando
+   * mediante un handlechange que seteara el valor ,
+   * por medio del input que estara observando los cambios del input que
+   * el incomveniente con esto es que cada vez que se haga una cambio 
+   * el componente se renderizara
+   */
 
-  /*
-  otra alternativa es aglomerar los fields en un objeto
   const handleSubmit = (e)=>{
     e.preventDefault();
-    const fields = Object.fromEntries(new window.FormData(e.target));
-    console.log(fields);
+    const {query} = Object.fromEntries(new window.FormData(e.target));
+    console.log(query);
   };
-*/
+
+  const handleChange= (e)=>{
+    setQuery(e.target.value);
+  }  
+
   return (
     <div className="page">
       <header>
         <h1>Buscador de péliculas</h1>
         <form className="form" onSubmit={handleSubmit}>
-          <input name="query" placeholder="Avengers, Star Wars, The Matrix" />
+          <input onChange={handleChange} value={query} name="query" placeholder="Avengers, Star Wars, The Matrix" />
           <button type="submit">Buscar</button>
         </form>
       </header>
