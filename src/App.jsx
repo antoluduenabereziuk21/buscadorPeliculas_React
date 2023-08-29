de44eb7ce502bc59f55ac4818da3a3a67b6d1935
antoluduenabereziuk21 <antolube20@gmail.com>
2023-08-28 21:18:03 -0300

    using useRef in form

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,36 @@
 import "./App.css";
 import { useMovies } from "./Hooks/useMovies";
 import { Movies } from "./components/Movies";
+import { useRef } from "react";
+/**
+ * use ref te permite guardar una referencia mutable
+ * que perciste durante todo el ciclo de vida del 
+ * componente.Es muy util para guardar cualkeir valor que 
+ * puedas guardar , y cada vez que cambia no renderiza
+ * nuevamente el componente
+ */
 
 function App() {
   const { movies: mappedMovies } = useMovies();
+  //lo utilizaremos para guardar una referencia del DOM
+  //en este caso lo usaremos en el input del form
+  const inputRef = useRef();
+  //cuando se haga click se llamara al handle 
+  //y sacaremos el valor de la referencia
+  const handleClick = (e)=>{
+    e.preventDefault();
+    const value = inputRef.current.value;
+    console.log(value);
+  };
+
 
   return (
     <div className="page">
       <header>
         <h1>Buscador de péliculas</h1>
         <form className="form">
-          <input placeholder="Avengers, Star Wars, The Matrix" />
-          <button type="submit">Buscar</button>
+          <input ref={inputRef} placeholder="Avengers, Star Wars, The Matrix" />
+          <button onClick={handleClick} type="submit">Buscar</button>
         </form>
       </header>
       <main>
