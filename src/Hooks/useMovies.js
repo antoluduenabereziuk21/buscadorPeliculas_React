de9e9ec632e030a9915db18c642ce72e226b9c3e
antoluduenabereziuk21 <antolube20@gmail.com>
2023-08-31 12:27:49 -0300

    implents useMemo en getMovies

--- src/Hooks/useMovies.js
+++ src/Hooks/useMovies.js
@@ -1,4 +1,4 @@
-import { useState,useRef,useMemo } from "react";
+import { useState, useRef, useMemo } from "react";
 import { searchMovies } from "../services/movies.service";
 /**
  * 
@@ -6,28 +6,29 @@ import { searchMovies } from "../services/movies.service";
  no tener que volver a calcular
  */
 
-
-export function useMovies({ search,sort }) {
+export function useMovies({ search, sort }) {
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const previousSearch = useRef(search);
-
-  const getMovies = async () => {
-    if(search === previousSearch.current) return
-    try {
-      setLoading(true);
-      setError(null);
-      previousSearch.current = search;
-      const newMovies = await searchMovies({ search });
-      setMovies(newMovies);
-    } catch (e) {
-      setError(e.message);
-    }
-    finally{
-      setLoading(false);
-    }
-  };
+//usamos useMemo para que se guarde en memoria el getMovies,
+//y cuando alla cambios en el search se renderize el componente
+  const getMovies = useMemo(() => {
+    return async () => {
+      if (search === previousSearch.current) return;
+      try {
+        setLoading(true);
+        setError(null);
+        previousSearch.current = search;
+        const newMovies = await searchMovies({ search });
+        setMovies(newMovies);
+      } catch (e) {
+        setError(e.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+  },[search]);
   /*const getSortedMovies = () => {
     console.log('getSortedMovies')
     const sortedMovies = sort
@@ -39,16 +40,15 @@ export function useMovies({ search,sort }) {
     return sortedMovies;
   }*/
   /**
-   * Este Calculo es algo que queremos memorizar 
+   * Este Calculo es algo que queremos memorizar
    * cosa que solamente se realize cuando cambie cierta info
    * en este caso el sort , o las movies
    */
   const sortedMovies = useMemo(() => {
-   
     return sort
-    ? [...movies].sort((a, b) => a.title.localeCompare(b.title))
-    :movies
-  },[sort,movies]);
+      ? [...movies].sort((a, b) => a.title.localeCompare(b.title))
+      : movies;
+  }, [sort, movies]);
 
-  return { movies: sortedMovies, getMovies,loading};
+  return { movies: sortedMovies, getMovies, loading };
 }
