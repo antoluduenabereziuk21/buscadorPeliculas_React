039cf9b1fb27d387e3c4f40a226dc8294762d4eb
antoluduenabereziuk21 <antolube20@gmail.com>
2023-08-31 12:57:31 -0300

    change useMemo for useCallBack on getMovies

--- src/Hooks/useMovies.js
+++ src/Hooks/useMovies.js
@@ -1,4 +1,4 @@
-import { useState, useRef, useMemo } from "react";
+import { useState, useRef, useMemo,useCallback } from "react";
 import { searchMovies } from "../services/movies.service";
 
 export function useMovies({ search, sort }) {
@@ -7,8 +7,11 @@ export function useMovies({ search, sort }) {
   const [error, setError] = useState(null);
   const previousSearch = useRef(search);
 
-  const getMovies = useMemo(() => {
-    return async ({search}) => {
+  //podriamos usar useCallBack, que nos simplifica la sintaxis,y usarlo solo para 
+  //las funciones,y por detras corre UseMemo
+
+   const getMovies = useCallback(
+    async ({search}) => {
       if (search === previousSearch.current) return;
       try {
         setLoading(true);
@@ -21,8 +24,25 @@ export function useMovies({ search, sort }) {
       } finally {
         setLoading(false);
       }
-    };
-  },[]);
+    },[]);
+
+
+  // const getMovies = useMemo(() => {
+  //   return async ({search}) => {
+  //     if (search === previousSearch.current) return;
+  //     try {
+  //       setLoading(true);
+  //       setError(null);
+  //       previousSearch.current = search;
+  //       const newMovies = await searchMovies({ search });
+  //       setMovies(newMovies);
+  //     } catch (e) {
+  //       setError(e.message);
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   };
+  // },[]);
 
   const sortedMovies = useMemo(() => {
     return sort
